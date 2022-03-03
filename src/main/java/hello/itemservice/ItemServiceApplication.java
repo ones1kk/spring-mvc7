74aabee23ab8d170b362ddf65c29b26119f4d6e6
ones1kk <kwsic@naver.com>
2022-03-03 22:54:30 +0900

    ✨ Add : @InitBinder

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -6,8 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ItemServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ItemServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ItemServiceApplication.class, args);
+    }
+
+    /*@Override
+    public Validator getValidator() {
+        return new ItemValidator();
+    }*/
 
 }
